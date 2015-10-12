2e988f58cded83d8b8086f3fe82623cb73ee595e
Nikolay Teslenko <nikolay.teslenko@logianalytics.com>
2015-10-12 18:05:53 +0300

    Test class

--- src/main/java/com/hlebnick/TestClass.java
+++ src/main/java/com/hlebnick/TestClass.java
@@ -1,5 +1,18 @@
 package com.hlebnick;
 
-public class TestClass {
+import java.util.Random;
 
+public class TestClass {
+	
+	private static final int SIZE = 100;
+	
+	public static void main(String[] args) {
+		Double inputArray[] = new Double[SIZE];
+		Random random = new Random();
+		
+		for (int i = 0; i < SIZE; i++) {
+			inputArray[i] = random.nextDouble();
+		}
+		System.out.println(inputArray.length);
+	}
 }
